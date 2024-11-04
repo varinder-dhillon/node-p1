673b6a51902665c164006d31ff3e3c345ca68d62
Varinder Dhillon <varinderdhilloninfo@gmail.com>
2024-11-04 21:41:33 +0530

    Created the http server

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const fs = require("fs");
+const http = require('http');
+
+/////////////////////////\
+// File reading
 
 // Read and write in synchronous (Blocking) code
 // const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
@@ -10,15 +14,28 @@ const fs = require("fs");
 
 // Read and write in Asynchronous (Non-Blocking) code
 
-fs.readFile("./txt/start.txt", "utf-8", (err, data1)=>{
-    console.log(data1);
-    fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2)=>{
-        console.log(data2);
-        fs.readFile("./txt/append.txt", "utf-8", (err, data3)=>{
-            console.log(data1);
-            fs.writeFile("./txt/append.txt",  `${data2}\n${data3}`, 'utf-8', err => {
-                console.log("File is written!!!", err);
-            })
-        })
-    })
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1)=>{
+//     console.log(data1);
+//     fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2)=>{
+//         console.log(data2);
+//         fs.readFile("./txt/append.txt", "utf-8", (err, data3)=>{
+//             console.log(data1);
+//             fs.writeFile("./txt/append.txt",  `${data2}\n${data3}`, 'utf-8', err => {
+//                 console.log("File is written!!!", err);
+//             })
+//         })
+//     })
+// })
+
+
+
+/////////////////////////\
+// server
+
+const server = http.createServer((req, res) => {
+    res.end("Hello from the backend server!!!")
+});
+
+server.listen(8000, "localhost", ()=>{
+    console.log("Listening requests on port 8000");
 })
